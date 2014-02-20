1b7ee4c10d92c0f2b6fb0284b48703e175ebf2bc
mithicfear <jamesdavidjackson1@gmail.com>
2014-02-20 15:52:24 -0600

    My commits aren't pushing first try for some reason, attempting to fix.

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,19 +15,26 @@ public class Main {
 
 	}
 	
+	@SuppressWarnings("deprecation")
 	public static void initialize() {
 		JFrame window = new JFrame("D&D Stuff!");
 		window.setSize(800, 600);
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		JButton butts = new JButton();
+		butts.setSize(100, 25);
+		butts.setLocation(800 / 2, 600 / 2);
 		butts.setText("THIS IS BUTTS");
 		JButton mensAsses = new JButton();
+		mensAsses.setSize(100, 25);
+		mensAsses.setLocation(800 / 2, 600/3);
 		mensAsses.setText("HARRY BUTTZ");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setLayout(JFrame.)
+		
 		window.add(butts);
 		window.add(mensAsses);
+		butts.show();
+		mensAsses.show();
 	}
 
 }
