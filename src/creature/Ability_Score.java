96f466b69ca4ce83e0074ab12ea3c45832e19029
Oggal <legoboy1122@yahoo.com>
2014-02-20 16:59:07 -0600

    It's being weird, says I can't merge mine and the fetch, Commiting everything, Mostly just a few container class's for now, will fill in later tonight.

---
+++ src/creature/Ability_Score.java
@@ -0,0 +1,24 @@
+package creature;
+
+public class Ability_Score {
+	
+	int STR;
+	int CON;
+	int DEX;
+	int INT;
+	int WIS;
+	int CHA;
+	
+	public Ability_Score(){
+		//TODO Randomly Gen ability scores to fill them in.
+	}
+	public Ability_Score(int str,int con,int dex, int intel,int wis,int cha){
+		STR=str;
+		CON= con;
+		DEX = dex;
+		INT = intel;
+		WIS = wis;
+		CHA = cha;
+	}
+
+}
