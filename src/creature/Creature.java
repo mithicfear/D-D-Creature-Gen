175b7b11fc18b62ed2deec70508eb30a460595d1
Oggal <legoboy1122@yahoo.com>
2014-02-20 17:30:30 -0600

    Filled in Creature and Ability_Score Classes

---
+++ src/creature/Creature.java
@@ -0,0 +1,22 @@
+package creature;
+
+public class Creature {
+
+	String Race;
+	String Class;
+	String Name;
+	String Alignment;
+	String Size;
+	
+	int Health;
+	int AC;
+	int FORT;
+	int REF;
+	int WILL;
+	int pass_Insight;
+	int pass_Perception;
+	
+	Ability_Score stats;
+	
+	
+}
