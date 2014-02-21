7c4f6b115379e259123248de59625e2d82c0fc47
mithicfear <jamesdavidjackson1@gmail.com>
2014-02-20 22:03:28 -0600

    What even is this crap, I can't push from EGit. BADSAIFHDSAKFNDSHAFHADN

--- src/creature/Creature.java
+++ src/creature/Creature.java
@@ -7,8 +7,8 @@ public class Creature {
 	String Name;
 	String Alignment;
 	String Size;
+	String Level;
 	
-	int Level;
 	int Health;
 	int AC;
 	int FORT;
@@ -19,18 +19,25 @@ public class Creature {
 	
 	Ability_Score stats;
 	
-	public Creature(Ability_Score as,int level,int r,int c){
-		//TODO Need to fill add dis stuff in, cause I'm lazy, Thinking of having a abstract class for race and class, staring them in an array so we can index them via an int.
-		if(as == null){
-			stats = new Ability_Score();
-		}else{
-			stats = as;
-		}
-		if(level == 0){
-			Level = 1;
-		}else{
-			Level = level;
-		}
+	public Creature(Object r, Object c, Object l) {
+		Race = r.toString();
+		Class = c.toString();
+		Level = l.toString();
+		
+		stats = new Ability_Score();
+		
+		calculateStats();
+		displayStats();
+	}
+	
+	/** This method exists to calculate everything that the DM will need for this NPC. */
+	protected void calculateStats() {
 		
 	}
+	
+	/** This method exists to display everything that the DM will need for this NPC. */
+	protected void displayStats() {
+		
+	}
+	
 }
--- src/main/Application.java
+++ src/main/Application.java
@@ -7,6 +7,11 @@
  */
 package main;
 
+import creature.Creature;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,23 +19,33 @@ import javax.swing.JPanel;
 
 public class Application {
 	
+	JComboBox races = new JComboBox();		//Define the combo boxes.
+	JComboBox classes = new JComboBox();
+	JComboBox levels = new JComboBox();
+	static JFrame window = new JFrame("D&D Stuff!"); //Define the JFrame.
+	static JPanel top = new JPanel();		//Define JPanels to put uneditable combo boxes on the screen.
+	static JPanel mid = new JPanel();
+	static JPanel bot = new JPanel();
+	
 	public Application(){ 	//This is the list of stages the program will go through in it's lifespan.
-		initialize();
-		update();
+		initializeFrame();
+		initializeBoxes();
 	}
 	
 	/** Creates JFrame, JPanels, and combo boxes. */
-	public static void initialize() {
-		JFrame window = new JFrame("D&D Stuff!");
+	public static void initializeFrame() {
+		
 		window.setSize(800, 600);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel top = new JPanel();		//Using JPanels to put uneditable combo boxes on the screen.
-		JPanel mid = new JPanel();
-		JPanel bot = new JPanel();
 		top.setBounds(0, 0, 800, 200);
 		mid.setBounds(0, 200, 800, 200);
 		bot.setBounds(0, 400, 800, 200);
+		window.setVisible(true);
+	}
+	
+	/** This method fills the boxes with information, */
+	protected void initializeBoxes() {
 		String[] levelNums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",	//Set up arrays for combo boxes.
 			"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
 			"25", "26", "27", "28", "29", "30", "31", "32"};		//Levels 1-32 (hehehe), Strings for all class and race names.
@@ -40,19 +55,37 @@ public class Application {
 		String[] raceNames = {"Deva", "Dragonborn", "Dwarf", "Eladrin", "Elf", "Githzerai",
 			"Gnome", "Goliath", "Half-Elf", "Half-Orc", "Halfling", "Human", "Minotaur",
 			"Shardmind", "Shifter", "Tiefling", "Wilden", };
-		JComboBox races = new JComboBox(raceNames);
-		JComboBox classes = new JComboBox(classNames);
-		JComboBox levels = new JComboBox(levelNums);
-		window.setVisible(true);
+		races.addItem(raceNames);
+		classes.addItem(classNames);
+		levels.addItem(levelNums);
+		races.setEditable(false);						//Make the boxes uneditable.
+		classes.setEditable(false);
+		levels.setEditable(false);
+		races.addActionListener((ActionListener) this);	//Give the boxes ActionListeners.
+		classes.addActionListener((ActionListener) this);
+		levels.addActionListener((ActionListener) this);
+		top.add(races);									//Add the boxes to the JPanels.
+		mid.add(classes);
+		bot.add(levels);
 	}
 	
-	protected static void update() {
+	/** This method reads what is in the boxes, and then creates a new creature based upon that information. */
+	protected void actionPerformed(ActionEvent e) {
+		JComboBox wat = (JComboBox)e.getSource();
+		Object r = null;
+		Object c = null;
+		Object l = null;
+		if(wat.equals(races))
+			r = races.getSelectedItem();
+		else if(wat.equals(classes))
+			c = classes.getSelectedItem();
+		else if(wat.equals(levels))
+			l = levels.getSelectedItem();
 		
-	}
-	
-	/** This gets input, may make class for this. */
-	protected static void collectData() {
+	  //if(r == null || c == null || l == null)
+			
 		
+		Creature herple = new Creature(r, c, l);
 	}
 	
 }
