15d52c905bee979a6a77a93c621f7e281f4be7d7
mithicfear <jamesdavidjackson1@gmail.com>
2014-02-26 14:01:50 -0600

    I FINISHED ALL THE MATHS! Need to find where to call the calc methods.

--- src/creature/Ability_Score.java
+++ src/creature/Ability_Score.java
@@ -1,6 +1,22 @@
+/**
+ * This is the Ability_Score class. This is used to calculate the ability scores. It calculates
+ * them by class, because of the fact that the classes have one skill that they are heavily
+ * based on. It takes the highest score that it gets and assigns it to that attribute.
+ * @author James Jackson
+ * @author Philip Taylor
+ */
 package creature;
 
 public class Ability_Score {
+	
+	/**
+	 * @param str
+	 * @param con
+	 * @param dex
+	 * @param intel
+	 * @param wis
+	 * @param cha
+	 */
 	
 	public int STR;
 	public int CON;
@@ -8,6 +24,7 @@ public class Ability_Score {
 	public int INT;
 	public int WIS;
 	public int CHA;
+	public int[] scoreArr = {STR, CON, DEX, INT, WIS, CHA};
 	
 	public Ability_Score() {
 		
@@ -16,15 +33,7 @@ public class Ability_Score {
 	public Ability_Score(String r, String c, int l){
 		//if (c.equals("Rogue") || ("") || ("") || (""))
 	}
-	/**
-	 * 
-	 * @param str
-	 * @param con
-	 * @param dex
-	 * @param intel
-	 * @param wis
-	 * @param cha
-	 */
+	
 	public Ability_Score(int str,int con,int dex, int intel,int wis,int cha){
 		STR = str;
 		CON = con;
@@ -34,28 +43,124 @@ public class Ability_Score {
 		CHA = cha;
 	}
 	
-	public void calculateStrClass() {
+	public int[] calculateStrClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[0] = scores[0];
+			
+		return scoreArr;
 	}
 	
-	public void calculateConClass() {
+	public int[] calculateConClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[1] = scores[0];
+			
+		return scoreArr;
 	}
 	
-	public void calculateDexClass() {
+	public int[] calculateDexClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[2] = scores[0];
+			
+		return scoreArr;
 	}
 	
-	public void calculateIntClass() {
+	public int[] calculateIntClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[3] = scores[0];
+			
+		return scoreArr;
 	}
 	
-	public void calculateWisClass() {
+	public int[] calculateWisClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[4] = scores[0];
+			
+		return scoreArr;
 	}
 	
-	public void calculateCharismaClass() {
+	public int[] calculateChaClass() {
+		int[] scores = new int[6];
 		
+		for (int x=0; x<5; x++)
+			scores[x] = (int) (Math.random() * 18);
+		
+		for (int u=0; u<5; u++)
+			for (int x=0; x<5; x++)
+				scores[x] = Math.max(scores[x], scores[x+1]);
+		
+		for (int x = 1; x<5; x++){
+			int r = (int)(Math.random()*5)+1;
+			scoreArr[x] = r;
+		}
+		
+		scoreArr[5] = scores[0];
+			
+		return scoreArr;
 	}
 
 }
